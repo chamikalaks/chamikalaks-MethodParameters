bbc539f5da56ed8e6f3e559a013019962e10e37a
chamikalaks <chamikalaks@gmail.com>
2023-02-20 19:08:18 +0000

    run hekko

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -12,7 +12,7 @@ public class Lab {
      * test, but will fail the other. This is because the test cases are setting up different scenarios to test the
      * functionality of this method - it will always expect the method to return the same variable that was provided
      * to the method. You can also try experimenting with the following guesses, as one of them will be the correct 
-     * answer. Please note that many of these will not compile, as evidenced by a "red line" under your code.
+     * answer. Please note that  of these will not compile, as evidenced by a "red line" under your code.
      *
      * return "hello"; | return String "hello";
      * return hello;   | return String hello;
